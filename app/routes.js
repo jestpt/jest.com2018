a7a4eca6227aac375eb75e2a65e60d11a787bb3d
Bruno Grifo <leitaogrifo@gmail.com>
2018-09-12 23:27:20 +0100

    action reload done

--- app/routes.js
+++ app/routes.js
@@ -13,24 +13,33 @@ var router = express.Router();
 //export our router
 module.exports = router;
 
+
+router.get('/home', function(req, res){
+    res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+router.post('/email',urlencodedParser, function(req, res){
+    // using SendGrid's v3 Node.js Library
+    // https://github.com/sendgrid/sendgrid-nodejs
+
+        const sgMail = require('@sendgrid/mail');
+        sgMail.setApiKey("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");
+        const msg = {
+            to: 'geral@jest.pt',
+            from: req.body.Email,
+            subject: 'Contacto jest.pt',
+            text: req.body.Message,
+        };
+        sgMail.send(msg);
+       //res.sendFile(path.join(__dirname, '../index.html'));
+       //res.send('root');
+       res.redirect('home');
+
+    
+});
+
 //route for our homepage
 router.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-
-
-router.post('/email',urlencodedParser, function(req, res){
-    // using SendGrid's v3 Node.js Library
-    // https://github.com/sendgrid/sendgrid-nodejs
-    const sgMail = require('@sendgrid/mail');
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const msg = {
-        to: 'geral@jest.pt',
-        from: req.body.Email,
-        subject: 'Contacto jest.pt',
-        text: req.body.Message,
-    };
-    sgMail.send(msg);
-    res.sendFile(path.join(__dirname, '../index.html'));
-});
